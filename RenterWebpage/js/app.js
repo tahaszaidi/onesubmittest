07919065c38f3677bd1b004e4102cfe7da692db2
fengxie98 <46989561+fengxie98@users.noreply.github.com>
2019-11-10 16:25:02 -0800

    Add files via upload
    An updated page for renter

---
+++ RenterWebpage/js/app.js
@@ -0,0 +1,29 @@
+function imageGallery() {
+	const highlight = document.querySelector(".gallery-highlight");
+	const previews = document.querySelectorAll(".room-preview img");
+	
+	previews.forEach(preview => {
+		preview.addEventListener('click', function(){
+			const smallSrc = this.src;
+			const bigSrc = smallSrc.replace("small", "big");
+			highlight.src = bigSrc;
+		});
+	}); 
+}
+
+imageGallery();
+
+/* function imageGallery2() {
+	const highlight = document.querySelector(".gallery-highlight2");
+	const previews = document.querySelectorAll(".room-preview2 img");
+	
+	previews.forEach(preview => {
+		preview.addEventListener('click', function(){
+			const smallSrc = this.src;
+			const bigSrc = smallSrc.replace("small", "big");
+			highlight.src = bigSrc;
+		});
+	}); 
+}
+
+imageGallery2(); */
